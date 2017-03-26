df839db2b8598bbb61ea2242b15c4962981d7d3d
Zachary Sang <zacharysang@gmail.com>
2017-03-26 17:32:32 -0400

    Added manifest.json, main.js, warning.css

---
+++ main.js
@@ -0,0 +1,5 @@
+var warning = document.createElement("div");
+var warningText = document.createTextNode("This page might be old");
+warning.appendChild(warningText);
+warning.setAttribute("id","best_before_warning");
+document.body.appendChild(warning);
