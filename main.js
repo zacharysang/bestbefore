b9bffd4a0edf90d9bbbfb6854631ef276c8dc0cf
Zachary Sang <zacharysang@gmail.com>
2017-03-26 18:26:29 -0400

    Added banner for when page is old

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ var warning = document.createElement("div");
 var warningText = document.createTextNode("This page might be old");
 warning.appendChild(warningText);
 warning.setAttribute("id","best_before_warning");
-document.body.appendChild(warning);
+warning.setAttribute("z-index","200");
+document.body.insertBefore(warning, document.body.firstChild);
