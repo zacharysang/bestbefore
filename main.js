d6aa43131583f4cfba33570026f86873c62e9ef2
Zachary Sang <zacharysang@gmail.com>
2017-03-26 23:04:00 -0400

    Started functionality for detecting age of page

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-var warning = document.createElement("div");
-var warningText = document.createTextNode("This page might be old");
-warning.appendChild(warningText);
-warning.setAttribute("id","best_before_warning");
-warning.setAttribute("z-index","200");
-document.body.insertBefore(warning, document.body.firstChild);
+if(true){
+    var warning = document.createElement("div");
+    var warningText = document.createTextNode("This page might be old");
+    warning.appendChild(warningText);
+    warning.setAttribute("id","best_before_warning");
+    warning.setAttribute("z-index","200");
+    document.body.insertBefore(warning, document.body.firstChild);
+}
+
+function oldAge(){
+    return 12;
+}
+
+function pageAge(){
+    var age = 0;
+    var dateRegEx = /[0-9]{,4}[\/\.\-][0-9]{,4}[\/\.\-][0-9]{,4}/;
+    if(document.baseURI.match(/[0-9]/))
+    return 2;
+}
