6638b658885d9881e2739ec48ffa8efb4063fb74
Zachary Sang <zacharysang@gmail.com>
2017-04-02 17:20:08 -0400

    Added momentjs functionality. Added checking for time element

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-if(pageAge() > oldAge()){
+if(pageAge().asMonths() > oldAge().asMonths()){
+
+    console.log(`page age: ${pageAge().asMonths()}, oldAge: ${oldAge().asMonths()}`)
     var warning = document.createElement("div");
     var warningText = document.createTextNode("This page might be old");
     warning.appendChild(warningText);
@@ -8,33 +10,41 @@ if(pageAge() > oldAge()){
 }
 
 function oldAge(){
-    return 12;
+    return moment.duration(12,'months');
 }
 
 function pageAge(){
-    var today = new Date();
-    var lastAuthored = new Date();
+    var today = moment();
+    var lastAuthored = moment();
     var yyyymm = /(([12]{1}[8901]{1})?[0-9]{2})[\.\-\/]{0,1}([01]{1}[0-9]{1})/;
     var mmyyyy = /([01]{1}[0-9]{1})[\.\-\/]{0,1}(([12]{1}[8901]{1})?[0-9]{2})/;
 
     //check for 'time' tag
+    var timeEl = document.getElementsByTagName("time");
+    if(timeEl){
+        lastAuthored = moment(timeEl[0].innerHTML);
+        console.log("time tag found");
+    }else{
+        console.log("No time tag found");
+    }
 
     //check meta tags for property containing 'date'
+    /*var metaEl = document.querySelectorAll("");*/
 
     //check other elements on page for hints
     
     //check URL for hints of last publish date
-    var yyyymmRes = yyyymmdd.exec(document.baseURI);
+    var yyyymmRes = yyyymm.exec(document.baseURI);
     var mmyyyyRes = mmyyyy.exec(document.baseURI);
-    if(yyyymmRes.length > 1){
+    if(yyyymmRes && yyyymmRes.length > 1){
        lastAuthored.setYear(yyyymmRes[1]);
        lastAuthored.setMonth(yyyymmRes[2]);
-    }else if(mmyyyyRes.length > 1){
+    }else if(mmyyyyRes && mmyyyyRes.length > 1){
        lastAuthored.setMonth(yyyymmRes[1]);
        lastAuthored.setYear(yyyymmRes[2]);
     }
     
-    var age = 12*(today.getYear()-lastAuthored.getYear()) + (today.getMonth() - lastAuthored.getMonth());
-    console.log("Page age: " + age);
+    var age = moment.duration(today.diff(lastAuthored));
+    console.log("Page age: " + age.asMonths());
     return age;
 }
