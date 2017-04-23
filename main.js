e1d21a9972c557bc0ba745550dd997ac4b5fd6d0
Zachary Sang <zacharysang@gmail.com>
2017-04-23 16:29:38 -0400

    Added shadow dom functionality. Solves #3

--- main.js
+++ main.js
@@ -1,34 +1,77 @@
-chrome.storage.sync.get('oldAge',function(result){
-        var configAge = result.oldAge || 12;
-        var oldAge = moment.duration(parseInt(configAge),'months');
-        if(pageAge() > oldAge){
-            displayWarning();
+// Declare styling for shadow dom compatibility
+const style = document.createElement('style');
+    style.innerText = `
+        #best_before_warning {
+        position: sticky;
+        width:100%;
+        top:0;
+        padding: 2.5rem;
+        color: white;
+        background-color: #D46a6a;
+        text-align: center;
+        font-size: 2rem;
+        z-index: 90000;
+        cursor: default;
+    }
+
+    #best_before_warning:hover {
+        opacity: 0.8;
+        transition: 0.3s;
         }
+
+    #best_before_warning:not(hover) {
+        opacity: 1;
+        transition: 0.25s;
+    }
+
+    .best_before_hidden {
+        transform: translate(-100%);
+        transition: 0.4s;
+    }
+    `
+
+chrome.storage.sync.get('oldAge', function (result) {
+    var configAge = result.oldAge || 12;
+    var oldAge = moment.duration(parseInt(configAge), 'months');
+    if (pageAge() > oldAge) {
+        displayWarning();
+    }
 });
 
 
-function displayWarning(){
+function displayWarning() {
     var warning = document.createElement("div");
-    warning.setAttribute('id','best_before_warning');
-    warning.setAttribute('z-index','200');
+    warning.setAttribute('id', 'best_before_warning');
+
     var warningText = document.createTextNode("This page might be old");
     warning.appendChild(warningText);
 
-    warning.addEventListener('click',function(ev){
-        
-        ev.target.addEventListener('transitionend', function(){
+    warning.addEventListener('click', function (ev) {
+        ev.target.addEventListener('transitionend', function () {
             ev.target.style.display = 'none';
         });
-        ev.target.setAttribute('class','best_before_hidden');
+        ev.target.setAttribute('class', 'best_before_hidden');
     })
-    
-    //check if shadow dom available, and if so, put it there
 
-    document.body.insertBefore(warning, document.body.firstChild);
+    //check if shadow dom available, and if so, put it there
+    if (document.body.createShadowRoot || document.body.attachShadow) {
+        var shadContainer = document.createElement('div');
+        document.body.insertBefore(shadContainer,document.body.firstChild);
+
+        var shadRoot = shadContainer.attachShadow({
+            mode: 'open'
+        });
+        shadRoot.appendChild(style);
+        shadRoot.appendChild(warning);
+    } else {
+        document.head.appendChild(style);
+        document.body.insertBefore(warning, document.body.firstChild);
+    }
+
 }
 
 
-function pageAge(){
+function pageAge() {
     var today = moment();
     var lastAuthored = today;
     var yyyymm = /(([12]{1}[8901]{1})?[0-9]{2})[\.\-\/]{0,1}([01]{1}[0-9]{1})/;
@@ -36,12 +79,12 @@ function pageAge(){
 
     //check for 'time' tag
     var timeEl = document.getElementsByTagName('time');
-    if(timeEl.length > 0){
+    if (timeEl.length > 0) {
         lastAuthored = moment(timeEl[0].innerHTML);
     }
 
     //check meta tags for property containing 'date'
-    /*var metaEl = document.querySelectorAll("");*/
+    var metaEl = document.querySelectorAll("[date*='']");
 
     //check other elements on page for hints
     /*
@@ -49,16 +92,16 @@ function pageAge(){
         * last-updated
         * 
      */
-        
-     /* This code needs reworking. need to handle case where we get multiple dates. 
-        Also need to handle dates existing in multiple sources
-        
-     var dateClassEls = document.querySelectorAll('[class*="date"]');
-     dateClassEls.forEach(function(el){
-        lastAuthored = moment(el.innerHTML);
-     });
-     */
-    
+
+    /* This code needs reworking. need to handle case where we get multiple dates. 
+       Also need to handle dates existing in multiple sources
+       
+    var dateClassEls = document.querySelectorAll('[class*="date"]');
+    dateClassEls.forEach(function(el){
+       lastAuthored = moment(el.innerHTML);
+    });
+    */
+
     //check URL for hints of last publish date
     /*var yyyymmRes = yyyymm.exec(document.baseURI);
     var mmyyyyRes = mmyyyy.exec(document.baseURI);
